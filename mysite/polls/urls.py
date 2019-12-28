c9383ed963ef1e727f75676268e2b558c8c6d3ac
donle <donle2044@gmail.com>
2019-12-28 08:27:38 +0700

    doncomit_finish_backend

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .import views
 urlpatterns = [
-    path('', views.viewlistloai,name="index"),
-    path('list/<int:loai_id>',views.viewlisttintuc,name="list")
+    path('index/', views.viewlistloai,name="index"),
+    path('list/<int:loai_id>',views.viewlisttintuc,name="list"),
+    path('tintuc/<int:tintuc_id>',views.viewtintuc,name="tintuc"),
+    path('',views.viewkey,name="key")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Theloai
 from .models import Tintuc
@@ -16,3 +16,17 @@ def viewlistloai(request):
 def viewlisttintuc(request, loai_id):
     listtt=Theloai.objects.get(pk=loai_id)
     return render(request,"polls/loaitintuc.html",{"tintuc":listtt})
+def viewtintuc(request, tintuc_id):
+    listloai=Theloai.objects.all()
+    listtt=get_object_or_404(Tintuc,pk=tintuc_id)
+    return render(request,"polls/tintuc.html",{"listloai":listloai,"tintuc":listtt})
+def viewkey(request):
+    #listloai=Theloai.objects.all()
+    #listtintuc=Tintuc.objects.all()
+    keytem=request.POST.get("keyget",'')
+    # for i in listtintuc:
+    #     if i.tieu_de.find(key)==-1:
+    #         listtintuc.remove(i)
+    # context={"listloai":listloai,"listtintuc":listtintuc}
+    return HttpResponse(keytem)
+    # return render(request,"polls/index.html",context)
